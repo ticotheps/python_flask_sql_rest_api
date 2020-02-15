dbb4dae3f136549fb646e6a6176b454f1d3e8b72
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-15 00:19:58 -0500

    Creates a 'notes.py' file in the root directory to log important learnings

---
+++ notes.py
@@ -0,0 +1,15 @@
+"""
+(1) What is `Flask`?
+    - Python web framework (i.e. - Djanjo)
+    
+(2) `Flask` vs `Django`
+    - Django
+        - High-level framework
+        - Very "opinionated"
+        - Comes "out-of-the-box" ready
+        - Has pre-determined way of accomplishing most tasks
+    - Flask
+        - Low-level framework
+        - Allows the developer more freedom and customization
+        - Similar in many ways to Node.js
+"""
