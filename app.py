38eaf1956aecba42d72a0ae704930ee6ac6880d0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-15 18:46:22 -0500

    Creates a 'Product()' class/model in 'app.py'

--- app.py
+++ app.py
@@ -17,6 +17,20 @@ db = SQLAlchemy(app)
 # Initializes Marshmallow (serializer + deserializer)
 ma = Marshmallow(app)
 
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+    
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
 # Runs the server
 if __name__ == '__main__':
     app.run(debug=True)
