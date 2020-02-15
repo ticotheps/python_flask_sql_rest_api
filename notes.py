850e539d01280cd1c5e300780c50806377863442
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-15 00:38:13 -0500

    Adds more learnings to 'notes.py' regarding: ORMs, ODMs, Marshmallow, etc.

--- notes.py
+++ notes.py
@@ -4,12 +4,32 @@
     
 (2) `Flask` vs `Django`
     - Django
-        - High-level framework
-        - Very "opinionated"
-        - Comes "out-of-the-box" ready
-        - Has pre-determined way of accomplishing most tasks
+        - High-level framework.
+        - Very "opinionated".        
+        - Comes ready to "plug-n-play" right "out-of-the-box".
+        - Contains pre-determined ways of accomplishing most tasks.
     - Flask
-        - Low-level framework
-        - Allows the developer more freedom and customization
-        - Similar in many ways to Node.js
+        - Low-level framework.
+        - Allows more freedom and customization with development.
+            - Similar in many ways to Node.js.
+
+(3) What is `SQLAlchemy`?
+    - An ORM (Object Relational Mapper)
+        - Used with Flask to manipulate the SQL database.
+        - Doesn't require raw SQL queries to manipulate the database (i.e. - 
+        Sequelize, Mongoose, built-in ORM included with Django).
+        - Much like just another abstraction layer between server and 
+        database.
+    
+(4) What is `Marshmallow`?
+    - An ORM (Object Relational Mapper) & An ODM (Object Document Mapper)
+        - Serializes and De-serializes Data.
+
+(5) What is `serialization` and `deserialization`?
+    - Serialization
+        - In the context of data storage, this is the process of translating data
+        structures or object state into formats that can be stored or transmitted
+        and reconstructed later.
+    - Deserialization
+        - The reconstructing of stored data into a human-readable format. 
 """
